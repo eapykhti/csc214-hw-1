c3aa30c5821f911507634c490958586cc037b189
Ella Apykhtin <mapykhti@u.rochester.edu>
2022-09-12 16:51:15 -0400

    Initial Commit

---
+++ Shared/hw_1App.swift
@@ -0,0 +1,17 @@
+//
+//  hw_1App.swift
+//  Shared
+//
+//  Created by Apykhtin, Mikhail on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct hw_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
